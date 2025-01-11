f1cec14c07ea5dc07211afd7fab50939702dd994
Ishaan Romil <ishaan.romil@gmail.com>
2025-01-11 13:59:07 +0530

    refactor: move to 3 letter ISO codes

--- dev/inconsistency.sh
+++ dev/inconsistency.sh
@@ -1,23 +1,23 @@
 TMPDIR=/tmp
 
-if [[ $1 = "es-fr" ]]; then
+if [[ $1 = "spa-fra" ]]; then
 
-lt-expand ../apertium-fr-es.es.dix | grep -v '<prn><enc>' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../apertium-fra-spa.spa.dix | grep -v '<prn><enc>' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer ../apertium-fr-es.es-fr.t1x  ../es-fr.t1x.bin  ../es-fr.autobil.bin |
-        apertium-interchunk ../apertium-fr-es.es-fr.t2x  ../es-fr.t2x.bin |
-        apertium-postchunk ../apertium-fr-es.es-fr.t3x  ../es-fr.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
-        lt-proc -d ../es-fr.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+        apertium-transfer ../apertium-fra-spa.spa-fra.t1x  ../spa-fra.t1x.bin  ../spa-fra.autobil.bin |
+        apertium-interchunk ../apertium-fra-spa.spa-fra.t2x  ../spa-fra.t2x.bin |
+        apertium-postchunk ../apertium-fra-spa.spa-fra.t3x  ../spa-fra.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../spa-fra.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
-elif [[ $1 = "fr-es" ]]; then
+elif [[ $1 = "fra-spa" ]]; then
 
-lt-expand ../fr.dix | grep -v '<prn><enc>' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
+lt-expand ../fra.dix | grep -v '<prn><enc>' | grep -e ':>:' -e '\w:\w' | sed 's/:>:/%/g' | sed 's/:/%/g' | cut -f2 -d'%' |  sed 's/^/^/g' | sed 's/$/$ ^.<sent>$/g' | tee $TMPDIR/tmp_testvoc1.txt |
         apertium-pretransfer|
-        apertium-transfer ../apertium-fr-es.fr-es.t1x  ../fr-es.t1x.bin  ../fr-es.autobil.bin |
-        apertium-interchunk ../apertium-fr-es.fr-es.t2x  ../fr-es.t2x.bin |
-        apertium-postchunk ../apertium-fr-es.fr-es.t3x  ../fr-es.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
-        lt-proc -d ../fr-es.autogen.bin > $TMPDIR/tmp_testvoc3.txt
+        apertium-transfer ../apertium-fra-spa.fra-spa.t1x  ../fra-spa.t1x.bin  ../fra-spa.autobil.bin |
+        apertium-interchunk ../apertium-fra-spa.fra-spa.t2x  ../fra-spa.t2x.bin |
+        apertium-postchunk ../apertium-fra-spa.fra-spa.t3x  ../fra-spa.t3x.bin  | tee $TMPDIR/tmp_testvoc2.txt |
+        lt-proc -d ../fra-spa.autogen.bin > $TMPDIR/tmp_testvoc3.txt
 paste -d _ $TMPDIR/tmp_testvoc1.txt $TMPDIR/tmp_testvoc2.txt $TMPDIR/tmp_testvoc3.txt | sed 's/\^.<sent>\$//g' | sed 's/_/   --------->  /g'
 
 
